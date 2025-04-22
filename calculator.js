6207ea24e6ec2a305434464fcab7281e5c87c3ce
Developer-Shuvo <im.computer.engineeer@gmail.com>
2025-04-22 14:27:42 +0600

    Not any number there is only string available

--- calculator.js
+++ calculator.js
@@ -1,39 +1,58 @@
-function add(num1, num2) {
-    return num1 + num2;
-}
+// function add(num1, num2) {
+//     return num1 + num2;
+// }
 
-function subtract(num1, num2) {
-    return num1 - num2;
-}
+// function subtract(num1, num2) {
+//     return num1 - num2;
+// }
 
+// function multiply(num1, num2) {
+//     return num1 * num2;
+// }
+
+// function devide(num1, num2) {
+//     return num1 / num2;
+// }
+
+
+// function calculation(a, b, operation) {
+//     if (operation === 'add') {
+//         let result = add(a, b);
+//         return result;
+//     } else if (operation === 'subtract') {
+//         let result = subtract(a, b);
+//         return result;
+//     } else if (operation === 'multiply') {
+//         let result = multiply(a, b);
+//         return result;
+//     } else if (operation === 'devide') {
+//         let result = devide(a, b);
+//         return result;
+//     } else {
+//         return "Invalid operation!";
+//     }
+
+// }
+
+
+// let result = calculation(10, 10, 'devide');
+// console.log('Your Calculation result is =', result);
+
+
+
+// *************************************************
 function multiply(num1, num2) {
-    return num1 * num2;
-}
 
-function devide(num1, num2) {
-    return num1 / num2;
-}
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return 'Invalid input! Please enter numbers';
 
-
-function calculation(a, b, operation) {
-    if (operation === 'add') {
-        let result = add(a, b);
-        return result;
-    } else if (operation === 'subtract') {
-        let result = subtract(a, b);
-        return result;
-    } else if (operation === 'multiply') {
-        let result = multiply(a, b);
-        return result;
-    } else if (operation === 'devide') {
-        let result = devide(a, b);
-        return result;
-    } else {
-        return "Invalid operation!";
     }
 
+    let result = num1 * num2;
+    return result;
 }
 
 
-let result = calculation(10, 10, 'devide');
-console.log('Your Calculation result is =', result);
+
+let result = multiply('10', 20);
+console.log("Your calculation result is =", result);
---
+++ validation.js
@@ -0,0 +1,29 @@
+// *if anyone given string in a summation then show "Please enter a number" message
+// *
+// **
+// function multiply(num1, num2) {
+
+//     if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+//         return 'Invalid input! Please enter numbers';
+//     }
+//     let calcultaion = num1 * num2;
+//     return calcultaion;
+// }
+
+
+// let result = multiply(10, 20);
+// console.log("Your calculation result is =", result);
+
+
+// ************************************************
+function createName(slot1, slot2, slot3) {
+    if (typeof slot1 !== 'string' || typeof slot2 !== 'string' || typeof slot3 !== 'string') {
+        return 'Invalid input! Please enter strings';
+    }
+    let nameIs = slot1 + " " + slot2 + " " + slot3;
+    return nameIs;
+}
+
+
+let result = createName('Nazmul', 'Hasan', 'Shuvo');
+console.log('Your name is =', result);
